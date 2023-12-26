9d58fddfbc162b39c4015705d83ea6e42e2172fe
jiayunh <132232846+jiayunh@users.noreply.github.com>
2023-12-26 12:44:44 +0800

    Update productioncode.py

--- productioncode.py
+++ productioncode.py
@@ -51,7 +51,7 @@ if file_content:
     df = pd.read_csv(BytesIO(file_content))
 
     # Display data in Streamlit app
-    st.title("<span style='color:darkblue; font-size:36px;'>Patch Cord Production App</span>", unsafe_allow_html=True)
+    st.title("Patch Cord Production App")
 
     # Calculate the date seven days ago
     today_minus_7_days = pd.to_datetime('today') - pd.DateOffset(days=7)
